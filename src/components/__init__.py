f8bfd72509c85d38c71e6e4e7bdca05894b666bf
asfiahamed0404 <muasfiahamed276@gmail.com>
2026-02-04 19:56:28 +0530

    logging and exception

---
+++ src/components/__init__.py
@@ -0,0 +1,2 @@
+## components it also create as package and use it in another file that is why using __init__.py
+# data ingestion is a part of module.  retrive data form dataset
---
+++ src/logger.py
@@ -0,0 +1,27 @@
+import logging
+import os
+from datetime import datetime
+
+# Generate log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Path to logs directory
+logs_dir = os.path.join(os.getcwd(), "logs")
+
+# Create logs directory if it doesn't exist
+os.makedirs(logs_dir, exist_ok=True)
+
+# Full path to the log file
+log_file_path = os.path.join(logs_dir, LOG_FILE)
+
+logging.basicConfig(
+    filename=log_file_path,               # Path to the log file where messages will be saved
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",  
+                                          # Format of each log line:
+                                          # %(asctime)s → timestamp
+                                          # %(lineno)d → line number in code
+                                          # %(name)s → logger name (usually 'root' by default)
+                                          # %(levelname)s → log level (INFO, ERROR, etc.)
+                                          # %(message)s → the actual log message
+    level=logging.INFO                    # Minimum severity level to log (INFO and above)
+)
