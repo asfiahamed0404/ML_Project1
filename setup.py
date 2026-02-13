0e5d33563cb1636e107e0bb6e95157e5e50b0d29
asfiahamed0404 <muasfiahamed276@gmail.com>
2026-02-13 18:32:32 +0530

    put some comments

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements(file_path:str)->list[str]:
     """
     requirements = []
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()      # Reads all lines from the file,Returns a list of strings,Each line ends with \n (newline character)
+        requirements = file_obj.readlines()      # Reads all lines from the file,Returns a list of strings,Each line ends with \n (newline character)  ["  numpy  ", " pandas\n", "\tscikit-learn  "]
         requirements = [req.strip() for req in requirements]  #requirements = [req.replace("\n","") for req in requirements] - old way
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -24,7 +24,7 @@ setup(
     version="0.0.1",
     author="Asfi",
     author_email="muasfiahamed276@gmail.com",
-    packages=find_packages(),
+    packages=find_packages(),   #it will directly go to all directory and see where __init__.py file is there and it will consider that as package and it will include that in the distribution package
     install_requires=get_requirements('requirements.txt')
 
 )
